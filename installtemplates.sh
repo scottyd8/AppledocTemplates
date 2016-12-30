55ad14c200ad6f9aa81ba03aada8a82e1fa77032
jkleinwaechter <joe.kleinwaechter@gmail.com>
2016-12-30 08:45:21 -0500

    Updated script to copy templateversion.sh

--- installtemplates.sh
+++ installtemplates.sh
@@ -1,11 +1,21 @@
 #!/bin/bash 
-cp -R -v ./templates/ ~/.appledoc/
-if [ $? -eq 0 ]
+cp -R ./templates/ ~/.appledoc/
+if [ $? -ne 0 ]
 then
-  echo "Worldpay Appledoc templates successfully installed"
-  exit 0
-else
   echo "Could not install Worldpay Appledoc templates." >&2
   echo "You can do this manually by copying the ./templates to ~/.appledoc/" >&2
+  echo "and ./teamplateversion.sh to ~/.appledoc/templateversion.sh." >&2
   exit 1
 fi
+cp ./templateversion.sh ~/.appledoc/
+if [ $? -ne 0 ]
+then
+  echo "Could not fully install Worldpay Appledoc templates." >&2
+  echo "You can finish this manually by copying " >&2
+  echo "./templateversion.sh to ~/.appledoc/templateversion.sh." >&2
+  exit 1
+fi
+
+echo "Templates successfully installed."
+exit 0
+
